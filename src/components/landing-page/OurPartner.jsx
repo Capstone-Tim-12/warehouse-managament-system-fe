bc8a9cc77577777c479a696bde30429dab22e020
Afitaganda <creatifypudding@gmail.com>
2023-11-11 18:59:35 +0700

    handling responsive mobile & tablet

--- src/components/landing-page/OurPartner.jsx
+++ src/components/landing-page/OurPartner.jsx
@@ -22,27 +22,29 @@ const OurPartner = () => {
       type: "Bisnis Kecil",
       total: "2 Gudang Disewa"
     },
-  ]
+  ];
 
   return (
-    <div className="bg-white flex flex-col p-8">
-      <h1 className="text-center text-cloud-burst-500 text-[28px] md:text-[38px] lg:text-[48px] font-bold w-full pb-12">
-        Mitra Kami
-      </h1>
-      <div className="grid grid-cols-3 gap-x-5 my-5">
-        {partnerList.map((item, index) => (
-          <div key={index} className="bg-cloud-burst-500 rounded-2xl flex flex-col items-center gap-5 p-4 text-white">
-            <img src={item.imgSrc} alt="" />
-            <div className="flex flex-col gap-2 items-center">
-              <p className="font-bold text-2xl">{item.name}</p>
-              <p className="font-normal text-lg">{item.type}</p>
+    <div className="bg-white">
+      <div className="max-w-screen-lg m-auto flex flex-col p-8">
+        <h1 className="text-center text-cloud-burst-500 text-[28px] md:text-[38px] lg:text-[48px] font-bold w-full pb-12">
+          Mitra Kami
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5 lg:grid-cols-3 lg:gap-5 gap-5 my-5">
+          {partnerList.map((item, index) => (
+            <div key={index} className="bg-cloud-burst-500 rounded-2xl flex flex-col items-center gap-5 p-4 text-white">
+              <img src={item.imgSrc} alt="" className="max-w-full h-auto" />
+              <div className="flex flex-col gap-2 items-center">
+                <p className="font-bold text-2xl">{item.name}</p>
+                <p className="font-normal text-lg">{item.type}</p>
+              </div>
+              <p className="font-bold text-xl">{item.total}</p>
             </div>
-            <p className="font-bold text-xl">{item.total}</p>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default OurPartner
+export default OurPartner;
--- src/components/landing-page/Testimonials.jsx
+++ src/components/landing-page/Testimonials.jsx
@@ -1,14 +1,44 @@
+import avatar1 from "../../assets/avatar-1.svg"
+import avatar2 from "../../assets/avatar-2.svg"
+
 import Carousel from "../global-component/Carousel"
 
-
 const Testimonials = () => {
+  const testimonialList = [
+    {
+      name: 'Rio Dewanto',
+      avatar: avatar1,
+      location: 'Jakarta, Indonesia',
+      message: 'Fitur perpesanan dalam aplikasi memastikan komunikasi lancar dan transparan. Ini adalah solusi terpadu yang menghemat waktu, menawarkan kenyamanan, dan menyediakan akses ke berbagai pilihan gudang.'
+    },
+    {
+      name: 'Nicholas Saputra',
+      avatar: avatar2,
+      location: 'Jakarta, Indonesia',
+      message: 'It has simplified my warehouse rental process and reduced my operational costs. This saves me so much time compared to the traditional way of searching for warehouse spaces.'
+    },
+  ]
+
   return (
-    <div className="bg-cloud-burst-50">
-      <h1 className="text-center text-cloud-burst-500 text-[28px] md:text-[38px] lg:text-[48px] font-bold w-full pb-12">
-        Testimoni
-      </h1>
-      <div className="flex flex-col items-center pb-16">
-        <Carousel/>
+    <div className="bg-cloud-burst-50 p-16">
+      <div className="max-w-screen-xl m-auto w-full">
+        <h1 className="text-center text-cloud-burst-500 text-[28px] md:text-[38px] lg:text-[48px] font-bold w-full pb-12">
+          Testimoni
+        </h1>
+        <div className="max-w-4xl m-auto p-8">
+          <Carousel slides={testimonialList.length}>
+            {testimonialList.map((testimoni, index) => (
+              <div key={index} className="!flex flex-col bg-white rounded-lg m-auto items-center justify-center p-12 gap-5">
+                <p className="text-xl text-center">{testimoni.message}</p>
+                <img src={testimoni.avatar} alt={testimoni.name} />
+                <div className="flex flex-col items-center justify-center">
+                  <p className="text-md font-bold text-center">{testimoni.name}</p>
+                  <p className="text-lg text-crusta-500 text-center">{testimoni.location}</p>
+                </div>
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </div>
     </div>
   )
