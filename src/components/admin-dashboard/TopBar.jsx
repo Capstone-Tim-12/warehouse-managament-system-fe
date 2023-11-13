250a4e73f11ddd233bfe81ca789f5c2b59d886e5
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-11-14 00:23:01 +0700

    add:dynamic title

--- src/components/admin-dashboard/TopBar.jsx
+++ src/components/admin-dashboard/TopBar.jsx
@@ -2,12 +2,12 @@ import searchIcon from "../../assets/search-icon.svg";
 import notificationIcon from "../../assets/notification-icon.svg";
 import userIcon from "../../assets/user-icon.svg";
 import { Link } from "react-router-dom";
-const TopBar = () => {
+const TopBar = ({ title }) => {
   return (
-    <div id="top-bar" className="bg-crusta-500 p-3">
-      <div className="flex items-center justify-between">
-        <h2 className="text-cloud-burst-500 text-[28px] font-bold">
-          Statistik Umum
+    <div id="top-bar" className="bg-crusta-500 p-1 lg:p-3">
+      <div className="flex flex-col items-center justify-between md:flex-row">
+        <h2 className="text-cloud-burst-500 text-[28px] font-bold mb-3 md:mb-0">
+          {title}
         </h2>
         <div className=" flex items-center gap-8">
           <form
@@ -17,13 +17,13 @@ const TopBar = () => {
             <input
               type="text"
               placeholder="Pencarian"
-              className="w-[300px]  focus:outline-none py-3 px-3 rounded-[28px] z-10 "
+              className="w-[180px] md:w-[300px]  focus:outline-none py-3 px-3 rounded-[28px]  "
             />
-            <button className="  px-3  ">
+            <button className="  pr-3  ">
               <img src={searchIcon} alt="search" />
             </button>
           </form>
-          <div className="flex items-center gap-6">
+          <div className="flex items-center gap-1 lg:gap-6">
             <Link>
               <img src={notificationIcon} alt="notification icon" />
             </Link>
