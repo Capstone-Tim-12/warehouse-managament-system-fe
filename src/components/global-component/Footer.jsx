2b711e57cd20e78710e637dd804d5b7a95713ea8
faozanumar <umarnur30@gamil.com>
2023-11-13 16:23:50 +0700

    responsive faq dan footer

--- src/components/global-component/Footer.jsx
+++ src/components/global-component/Footer.jsx
@@ -8,118 +8,115 @@ import Alamat from "../../assets/alamat.svg";
 
 
 const Footer = () => {
-    return (
-        <div className="footer-container bg-cloud-burst-500 text-white">
-            <div className="footer-content ">
-                <div className="grid grid-cols-3 ">
-                    <div className="me-3 mb-3 ">
-                        <img
-                            src={Logo}
-                            alt="Logo"
-                            width="80px"
-                            height="60px"
-                            style={{ verticalAlign: "middle", display: "inline-block" }}
-                            className="mb-4"
-                        />
-                        <h4
-                            className="mb-4 font-bold"
-                            style={{
-                                display: "inline-block",
-                                verticalAlign: "middle",
-                                marginTop: "px",
-                            }}
-                        >
-                            DigiHouse
-                        </h4>
-                        <p className="mb-4 font-semibold">Sewa Gudang dengan Mudah</p>
-                        <p className="mb-4">Akses pengguna 24 jam</p>
-                        <div className="icon flex gap-3 mt-3 justify">
-                            <a href="">
-                                <img src={Instagram} alt="Instagram Icon" />
-                            </a>
-                            <a href="">
-                                <img src={Facebook} alt="Facebook Icon" />
-                            </a>
-                        </div>
-                    </div>
-                    <div  style={{ marginTop: '24px' }}>
-                        <h2 className="mb-6">Benefit</h2>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="benefit1" className="hidden" />
-                            <label htmlFor="benefit1" className="flex items-center">
-                                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
-                                Tempat nyaman dan strategis
-                            </label>
-                        </div>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="benefit2" className="hidden" />
-                            <label htmlFor="benefit2" className="flex items-center">
-                                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
-                                Keamanan terjaga
-                            </label>
-                        </div>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="benefit3" className="hidden" />
-                            <label htmlFor="benefit3" className="flex items-center">
-                                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
-                                Menyediakan ruang banyakMenyediakan ruang banyak
-                            </label>
-                        </div>
-                    </div>
-                    <div style={{ marginTop: '24px' }}>
-                        <h2 className="mb-6">Contact us</h2>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="contact1" className="hidden" />
-                            <label htmlFor="contact1" className="flex items-center">
-                                <img src={Telp} alt="Telp Icon" className="mr-3" />
-                                Nomor Telp: 083134422346
-                            </label>
-                        </div>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="contact2" className="hidden" />
-                            <label htmlFor="contact2" className="flex items-center">
-                                <img src={Email} alt="Email Icon" className="mr-3" />
-                                Email: digihouse@gmail.com
-                            </label>
-                        </div>
-                        <div className="checklist-item flex items-center mb-6">
-                            <input type="checkbox" id="contact3" className="hidden" />
-                            <label htmlFor="contact3" className="flex items-center">
-                                <img src={Alamat} alt="Alamat Icon" className="mr-3" />
-                                Alamat: Jl. Gunawarman No. 21
-                            </label>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="footer-container bg-cloud-burst-500 text-white py-3">
+      <div className="footer-content px-2 max-w-[1200px] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+          <div className="me-3 mb-3 mt-10">
+            <img
+              src={Logo}
+              alt="Logo"
+              width="80px"
+              height="60px"
+              style={{ verticalAlign: "middle", display: "inline-block" }}
+              className="mb-4"
+            />
+            <h4
+              className="mb-4 font-bold"
+              style={{
+                display: "inline-block",
+                verticalAlign: "middle",
+                marginTop: "px",
+              }}
+            >
+              DigiHouse
+            </h4>
+            <p className="mb-4 font-semibold">Sewa Gudang dengan Mudah</p>
+            <p className="mb-4">Akses pengguna 24 jam</p>
+            <div className="icon flex gap-3 mt-3 justify">
+              <a href="">
+                <img src={Instagram} alt="Instagram Icon" />
+              </a>
+              <a href="">
+                <img src={Facebook} alt="Facebook Icon" />
+              </a>
             </div>
-            {/* Copyright and Logo */}
-            <div className="mt-5 mx-5">
-                <p style={{ textAlign: "center", fontWeight: "600" }}>
-                    &copy; Warehouse Rental App 2023 - All rights reserved
-                    <a
-                        href="/privacy-policy"
-                        style={{
-                            marginLeft: "60px",
-                            marginRight: "60px",
-                            fontWeight: "600",
-                        }}
-                    >
-                        Privacy Policy
-                    </a>
-                    <a
-                        href="/terms-of-service"
-                        style={{ marginLeft: "60px", fontWeight: "600" }}
-                    >
-                        Terms of Service
-                    </a>
-                </p>
+          </div>
+          <div style={{ marginTop: "24px" }}>
+            <h2 className="mb-6">Benefit</h2>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="benefit1" className="hidden" />
+              <label htmlFor="benefit1" className="flex items-center">
+                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
+                Tempat nyaman dan strategis
+              </label>
             </div>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="benefit2" className="hidden" />
+              <label htmlFor="benefit2" className="flex items-center">
+                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
+                Keamanan terjaga
+              </label>
+            </div>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="benefit3" className="hidden" />
+              <label htmlFor="benefit3" className="flex items-center">
+                <img src={Checklist} alt="Checklist Icon" className="mr-3" />
+                Menyediakan ruang banyak Menyediakan ruang banyak
+              </label>
+            </div>
+          </div>
+          <div style={{ marginTop: "24px" }}>
+            <h2 className="mb-6">Contact us</h2>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="contact1" className="hidden" />
+              <label htmlFor="contact1" className="flex items-center">
+                <img src={Telp} alt="Telp Icon" className="mr-3" />
+                Nomor Telp: 083134422346
+              </label>
+            </div>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="contact2" className="hidden" />
+              <label htmlFor="contact2" className="flex items-center">
+                <img src={Email} alt="Email Icon" className="mr-3" />
+                Email: digihouse@gmail.com
+              </label>
+            </div>
+            <div className="checklist-item flex items-center mb-6">
+              <input type="checkbox" id="contact3" className="hidden" />
+              <label htmlFor="contact3" className="flex items-center">
+                <img src={Alamat} alt="Alamat Icon" className="mr-3" />
+                Alamat: Jl. Gunawarman No. 21
+              </label>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      {/* Copyright and Logo */}
+      <div className="mt-5 mx-5">
+        <p style={{ textAlign: "center", fontWeight: "600" }}>
+          &copy; Warehouse Rental App 2023 - All rights reserved
+          <a
+            href="/privacy-policy"
+            style={{
+              marginLeft: "60px",
+              marginRight: "60px",
+              fontWeight: "600",
+            }}
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="/terms-of-service"
+            style={{ marginLeft: "60px", fontWeight: "600" }}
+          >
+            Terms of Service
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 
 export default Footer;
-
-
-
--- src/components/landing-page/Faq.jsx
+++ src/components/landing-page/Faq.jsx
@@ -1,33 +1,64 @@
-import React from 'react';
+import React from "react";
 import hero3 from "../../assets/hero3-image.svg";
+
 
 const Faq = () => {
   const faqData = [
-    { question: 'Apa itu penyewaan gudang?', answer: 'Penyewaan gudang adalah proses dimana seseorang atau perusahaan menyewa ruang penyimpanan untuk menyimpan barang atau inventaris mereka.' },
-    { question: 'Berapa biaya penyewaan gudang?', answer: 'Biaya penyewaan gudang dapat bervariasi tergantung pada lokasi, ukuran, fasilitas, dan permintaan di pasar. Harga dapat berupa biaya bulanan atau tahunan.' },
-    { question: 'Bagaimana cara menemukan gudang yang sesuai untuk disewa?', answer: 'Anda dapat mencari gudang yang sesuai untuk disewa melalui agen real estat, situs web penyewaan komersial, atau menghubungi pemilik gudang secara langsung.' },
-    { question: 'Bagaimana memastikan keamanan barang saya di gudang yang disewa?', answer: 'Pastikan gudang memiliki fasilitas keamanan seperti kamera pengawas, akses terbatas, dan sistem alarm. Selain itu, pertimbangkan untuk memiliki asuransi penyimpanan untuk barang Anda.' },
-    { question: 'Apa yang perlu dipertimbangkan sebelum menyewa gudang?', answer: 'Faktor yang perlu dipertimbangkan termasuk lokasi, ukuran gudang, biaya sewa, fasilitas keamanan, durasi sewa, dan persyaratan kontrak.' },
-    { question: 'Apa yang dimaksud penyewaan gudang komersial?', answer: 'Penyewaan gudang komersial biasanya digunakan untuk bisnis dan mungkin memiliki fasilitas tambahan seperti kantor.' },
+    {
+      question: "Apa itu penyewaan gudang?",
+      answer:
+        "Penyewaan gudang adalah proses dimana seseorang atau perusahaan menyewa ruang penyimpanan untuk menyimpan barang atau inventaris mereka.",
+    },
+    {
+      question: "Berapa biaya penyewaan gudang?",
+      answer:
+        "Biaya penyewaan gudang dapat bervariasi tergantung pada lokasi, ukuran, fasilitas, dan permintaan di pasar. Harga dapat berupa biaya bulanan atau tahunan.",
+    },
+    {
+      question: "Bagaimana cara menemukan gudang yang sesuai untuk disewa?",
+      answer:
+        "Anda dapat mencari gudang yang sesuai untuk disewa melalui agen real estat, situs web penyewaan komersial, atau menghubungi pemilik gudang secara langsung.",
+    },
+    {
+      question:
+        "Bagaimana memastikan keamanan barang saya di gudang yang disewa?",
+      answer:
+        "Pastikan gudang memiliki fasilitas keamanan seperti kamera pengawas, akses terbatas, dan sistem alarm. Selain itu, pertimbangkan untuk memiliki asuransi penyimpanan untuk barang Anda.",
+    },
+    {
+      question: "Apa yang perlu dipertimbangkan sebelum menyewa gudang?",
+      answer:
+        "Faktor yang perlu dipertimbangkan termasuk lokasi, ukuran gudang, biaya sewa, fasilitas keamanan, durasi sewa, dan persyaratan kontrak.",
+    },
+    {
+      question: "Apa yang dimaksud penyewaan gudang komersial?",
+      answer:
+        "Penyewaan gudang komersial biasanya digunakan untuk bisnis dan mungkin memiliki fasilitas tambahan seperti kantor.",
+    },
   ];
 
+
   return (
-    <div className="bg-cloud-burst-50">
+    <div className="bg-cloud-burst-50 px-2">
       <img src={hero3} alt="Hero3" className="mx-auto " />
-      <div className="text-3xl font-bold mb-4 text-center bg-cloud-burst-500 text-white p-6 rounded-[20px] w-[770px] h-[85px] mx-auto ">Pertanyaan yang Sering Diajukan</div>
-      <div className="grid gap-4 grid-cols-2 mr-[20px] ml-[40px] py-10">
+      <div className=" font-bold mb-4 text-center bg-cloud-burst-500 text-white p-6 rounded-[20px] w-[300px] mx-auto md:w-[700px] md:text-2xl h-[85px]  ">
+        Pertanyaan yang Sering Diajukan
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 place-items-center  max-w-[1000px] mx-auto py-10">
         {faqData.map((item, index) => (
-          
-          <div key={index} className="bg-white p-4 rounded-[14px] relative border-l-[14px] border-cloud-burst-500 w-[612px] h-[190px] ">
-          <h3 className="text-xl font-bold mb-2">{item.question}</h3>
-          <div className="absolute top-4 right-4 w-4 h-4 bg-cloud-burst-500"></div>
-          <p>{item.answer}</p>
-        </div>
-        
+          <div
+            key={index}
+            className="bg-white p-4 rounded-[14px] relative border-l-[14px] border-cloud-burst-500  "
+          >
+            <h3 className="text-xl font-bold mb-2 px-2">{item.question}</h3>
+            <div className="absolute top-2 right-2 w-4 h-4 bg-cloud-burst-500"></div>
+            <p className="px-2">{item.answer}</p>
+          </div>
         ))}
       </div>
     </div>
   );
 };
+
 
 export default Faq;
