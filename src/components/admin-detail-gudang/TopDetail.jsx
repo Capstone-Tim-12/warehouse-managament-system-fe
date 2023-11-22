a6a66cd89ee90c71ce0f7b0e02664d6ec1d0a266
Fikriwib12 <wibfikri@gmail.com>
2023-11-22 13:03:54 +0700

    fix icon detail gudang

--- src/components/admin-detail-gudang/TopDetail.jsx
+++ src/components/admin-detail-gudang/TopDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import IconEdit from "../../assets/icons-edit.svg";
+import IconEdit from "../../assets/Icons-edit.svg";
 
 const TopDetail = () => {
   return (
@@ -9,7 +9,7 @@ const TopDetail = () => {
           Detail Warehouse
         </h1>
         <button className="bg-orange-500 h-[54px] px-2 sm:px-4 sm:py-3 rounded-lg flex  items-center ">
-          {/* <img src={IconEdit} alt="IconEdit" className="w-8 h-8" /> */}
+          <img src={IconEdit} alt="IconEdit" className="w-8 h-8" />
           <span className="text-white ml-2 ">Edit Gudang</span>
         </button>
       </div>
