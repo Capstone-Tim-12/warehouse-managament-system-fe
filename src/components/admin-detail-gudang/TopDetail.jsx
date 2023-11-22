758ab1aa8ea3b50206a2efc5bacb532d23cdaa40
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-11-22 12:49:59 +0700

    deggubing assets detail gudang

--- src/components/admin-detail-gudang/TopDetail.jsx
+++ src/components/admin-detail-gudang/TopDetail.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import IconEdit from '../../assets/icons-edit.svg';
+import React from "react";
+// import IconEdit from "../../assets/icons-edit.svg";
 
 const TopDetail = () => {
   return (
     <>
-        <div className='flex w-full h-full justify-between items-center '>
-            <h1 className='text-[20px] font-bold text-cloud-burst-500'>Detail Warehouse</h1>
-            <button className='bg-orange-500 h-[54px] px-2 sm:px-4 sm:py-3 rounded-lg flex  items-center '>
-                <img src={IconEdit} alt="IconEdit" className='w-8 h-8' />
-                <span className='text-white ml-2 '>Edit Gudang</span>
-            </button>
-        </div>
+      <div className="flex w-full h-full justify-between items-center ">
+        <h1 className="text-[20px] font-bold text-cloud-burst-500">
+          Detail Warehouse
+        </h1>
+        <button className="bg-orange-500 h-[54px] px-2 sm:px-4 sm:py-3 rounded-lg flex  items-center ">
+          {/* <img src={IconEdit} alt="IconEdit" className="w-8 h-8" /> */}
+          <span className="text-white ml-2 ">Edit Gudang</span>
+        </button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default TopDetail
+export default TopDetail;
