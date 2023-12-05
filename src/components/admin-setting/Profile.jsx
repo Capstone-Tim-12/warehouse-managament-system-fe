da1492dfca4a939350b88645fe67adc99bc49464
Afitaganda <creatifypudding@gmail.com>
2023-12-05 14:41:08 +0700

    update input style admin setting

--- src/components/admin-setting/Profile.jsx
+++ src/components/admin-setting/Profile.jsx
@@ -1,35 +1,60 @@
 import userIcon from '../../assets/user-setting-icon.svg'
 import emailIcon from '../../assets/icon-email-setting.svg'
+import { useState } from 'react'
 
 const Profile = () => {
+    const [profile, setProfile] = useState({
+        user: '',
+        email: '',
+        address: '',
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setProfile({ ...profile, [name]: value })
+    }
     return (
         <div className="flex flex-col gap-11">
-            <div className="flex flex-row items-center m-auto py-2 px-4 gap-2 border border-cloud-burst-500 rounded-lg w-full">
+            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.user !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={userIcon} alt="" />
                 </div>
                 <div className="flex flex-col w-full">
                     <label htmlFor="name" className="text-xs">Name</label>
                     <input type="text"
-                        className="focus:outline-none"
-                        placeholder="Elon Musk" />
+                        id='inpt-setting-username'
+                        name='user'
+                        value={profile.user}
+                        onChange={handleChange}
+                        className="focus:outline-none border border-none focus:ring-0 "
+                        placeholder="" />
                 </div>
             </div>
-            <div className="flex flex-row items-center m-auto py-2 px-4 gap-2 border border-cloud-burst-500 rounded-lg w-full">
+            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.email !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={emailIcon} alt="" />
                 </div>
                 <div className="flex flex-col w-full">
                     <label htmlFor="email" className="text-xs">Email</label>
                     <input type="email"
+                    id='inpt-setting-email'
+                        name='email'
+                        value={profile.email}
+                        onChange={handleChange}
                         className="focus:outline-none"
-                        placeholder="fauzi@example.com" />
+                        placeholder="" />
                 </div>
             </div>
-            <div className="m-auto border border-cloud-burst-500 rounded-lg w-full h-28">
-                <textarea name="address" id="" placeholder="Alamat" className="px-4 py-2 w-full h-full focus:outline-none rounded-lg"></textarea>
+            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.address !== '' && 'border-cloud-burst-500 border'}`}>
+                <textarea
+                    name="address"
+                    id="inptSettingAddress"
+                    value={profile.address}
+                    onChange={handleChange}
+                    placeholder="Alamat"
+                    className="px-4 py-2 w-full h-full focus:outline-none rounded-lg"></textarea>
             </div>
-            <div className="flex justify-end">
+            <div className="flex justify-start">
                 <button id="save-profile-setting"
                     className="bg-crusta-500 text-white flex justify-center items-center h-10 w-72 rounded-lg hover:bg-crusta-600">
                     Simpan
--- src/components/admin-setting/Security.jsx
+++ src/components/admin-setting/Security.jsx
@@ -1,32 +1,50 @@
+import { useState } from 'react'
 import emailIcon from '../../assets/icon-email-setting.svg'
 import passIcon from '../../assets/icon-pass-setting.svg'
 
 const Security = () => {
+    const [account, setAccount] = useState({
+        email: '',
+        password: '',
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setAccount({...account, [name]:value})
+    }
     return (
         <div className="flex flex-col gap-11">
-            <div className="flex flex-row items-center m-auto py-2 px-4 gap-2 border border-cloud-burst-500 rounded-lg w-full">
+            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${account.email !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={emailIcon} alt="" />
                 </div>
                 <div className="flex flex-col w-full">
                     <label htmlFor="email" className="text-xs">Email</label>
                     <input type="text"
+                    name='email'
+                    value={account.email}
+                    id='inpt-setting-secure-email'
+                    onChange={handleChange}
                         className="focus:outline-none"
-                        placeholder="Elon Musk" />
+                        placeholder="" />
                 </div>
             </div>
-            <div className="flex flex-row items-center m-auto py-2 px-4 gap-2 border border-cloud-burst-500 rounded-lg w-full">
+            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${account.password !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={passIcon} alt="" />
                 </div>
                 <div className="flex flex-col w-full">
                     <label htmlFor="password" className="text-xs">Password</label>
                     <input type="password"
+                    name='password'
+                    value={account.password}
+                    id='inpt-setting-secure-password'
+                    onChange={handleChange}
                         className="focus:outline-none"
-                        placeholder="Elon Musk" />
+                        placeholder="" />
                 </div>
             </div>
-            <div className="flex justify-end">
+            <div className="flex justify-start">
                 <button id="save-profile-setting"
                     className="bg-crusta-500 text-white flex justify-center items-center h-10 w-72 rounded-lg hover:bg-crusta-600">
                     Simpan
