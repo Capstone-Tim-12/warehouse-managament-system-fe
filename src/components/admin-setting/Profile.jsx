bd0aad9a0a63eeec03dd47905f1141bb262244cb
Afitaganda <creatifypudding@gmail.com>
2023-12-07 16:27:29 +0700

    handle floating label for input setting

--- src/components/admin-setting/Profile.jsx
+++ src/components/admin-setting/Profile.jsx
@@ -15,44 +15,51 @@ const Profile = () => {
     }
     return (
         <div className="flex flex-col gap-11">
-            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.user !== '' && 'border-cloud-burst-500 border'}`}>
+            <div className={`flex flex-row items-center m-auto px-4 gap-2 h-16 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.user !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={userIcon} alt="" />
                 </div>
-                <div className="flex flex-col w-full">
-                    <label htmlFor="name" className="text-xs">Name</label>
-                    <input type="text"
-                        id='inpt-setting-username'
-                        name='user'
-                        value={profile.user}
-                        onChange={handleChange}
-                        className="focus:outline-none border border-none focus:ring-0 "
-                        placeholder="" />
+                <div className="flex flex-col w-full relative">
+                    <label className="relative cursor-pointer mt-1">
+                        <input type="text"
+                            id='inpt-setting-username'
+                            name='user'
+                            value={profile.user}
+                            onChange={handleChange}
+                            className="focus:outline-none border border-none transition duration-200 placeholder-opacity-0 placeholder-gray-300"
+                            placeholder="Name" />
+                        <span className='absolute left-0 transition duration-200 label-input'>Name</span>
+                    </label>
                 </div>
             </div>
-            <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.email !== '' && 'border-cloud-burst-500 border'}`}>
+            <div className={`flex flex-row items-center m-auto px-4 gap-2 h-16 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.email !== '' && 'border-cloud-burst-500 border'}`}>
                 <div>
                     <img src={emailIcon} alt="" />
                 </div>
-                <div className="flex flex-col w-full">
-                    <label htmlFor="email" className="text-xs">Email</label>
-                    <input type="email"
-                    id='inpt-setting-email'
-                        name='email'
-                        value={profile.email}
-                        onChange={handleChange}
-                        className="focus:outline-none"
-                        placeholder="" />
+                <div className="flex flex-col w-full relative">
+                    <label className="relative cursor-pointer mt-1">
+                        <input type="email"
+                            id='inpt-setting-email'
+                            name='email'
+                            value={profile.email}
+                            onChange={handleChange}
+                            className="focus:outline-none border border-none transition duration-200 placeholder-opacity-0 placeholder-gray-300"
+                            placeholder="Email" />
+                        <span className='absolute left-0 transition duration-200 label-input'>Email</span>
+                    </label>
                 </div>
             </div>
             <div className={`flex flex-row items-center m-auto py-2 px-4 gap-2 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.address !== '' && 'border-cloud-burst-500 border'}`}>
-                <textarea
-                    name="address"
-                    id="inptSettingAddress"
-                    value={profile.address}
-                    onChange={handleChange}
-                    placeholder="Alamat"
-                    className="px-4 py-2 w-full h-full focus:outline-none rounded-lg"></textarea>
+                <label className="relative cursor-pointer mt-4 w-full">
+                    <textarea
+                        name="address"
+                        id="inptSettingAddress"
+                        value={profile.address}
+                        onChange={handleChange}
+                        placeholder="Alamat"
+                        className="w-full h-full rounded-lg focus:outline-none border border-none transition duration-200 placeholder-opacity-0 placeholder-gray-300"></textarea>
+                    <span className='absolute left-0 transition duration-200 label-input'>Alamat</span>
+                </label>
             </div>
             <div className="flex justify-start">
                 <button id="save-profile-setting"
