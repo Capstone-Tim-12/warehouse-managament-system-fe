af4ace556c66909379c5c8a738286f6089898d7c
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-12-08 00:49:18 +0700

    add: loading effect for dashboard recent customer

--- src/components/admin-dashboard/RecentCustomer.jsx
+++ src/components/admin-dashboard/RecentCustomer.jsx
@@ -5,6 +5,7 @@ import Cookies from "js-cookie";
 
 const RecentCustomer = () => {
   const [recentCustomer, setRecentCustomer] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const handleRecentCustomer = () => {
     const token = Cookies.get("token");
@@ -19,9 +20,11 @@ const RecentCustomer = () => {
       )
       .then((response) => {
         setRecentCustomer(response?.data?.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false);
       });
   };
 
@@ -32,24 +35,30 @@ const RecentCustomer = () => {
   return (
     <div className="bg-white rounded-[8px] p-3">
       <h3 className="text-[20px] font-bold">Recent Customer</h3>
-      <div className="  flex flex-col gap-5">
-        {recentCustomer.slice(0, 4).map((item) => {
-          return (
-            <div
-              className=" bg-cloud-burst-200 w-full h-auto p-3 rounded-[12px] flex items-center gap-4 border border-black"
-              key={item.transactionId}
-            >
-              <div className="bg-cloud-burst-500 w-[30px] h-[30px] flex justify-center items-center text-white rounded-full">
-                <h5>{item.username.charAt(0).toUpperCase()}</h5>
+
+      {loading ? (
+        <p className="text-xl py-5">Memuat data...</p>
+      ) : (
+        <div className="  flex flex-col gap-5">
+          {recentCustomer.slice(0, 4).map((item) => {
+            return (
+              <div
+                className=" bg-cloud-burst-200 w-full h-auto p-3 rounded-[12px] flex items-center gap-4 border border-black"
+                key={item.transactionId}
+              >
+                <div className="bg-cloud-burst-500 w-[30px] h-[30px] flex justify-center items-center text-white rounded-full">
+                  <h5>{item.username.charAt(0).toUpperCase()}</h5>
+                </div>
+                <div className="">
+                  <p>{item.username}</p>
+                  <p>{item.regencyName}</p>
+                </div>
               </div>
-              <div className="">
-                <p>{item.username}</p>
-                <p>{item.regencyName}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
+
       <button className="flex items-center gap-5">
         Lihat Selengkapnya
         <img src={rightArrowIcon} alt="right arrow" />
