916b969f12e1e3762652072d8c512f65055f1359
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-12-07 20:43:19 +0700

    consume api recent customer dashboard

--- src/components/admin-dashboard/RecentCustomer.jsx
+++ src/components/admin-dashboard/RecentCustomer.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const RecentCustomer = () => {
-  const [recentCustomer, setRecentCustomer] = useState({});
+  const [recentCustomer, setRecentCustomer] = useState([]);
 
   const handleRecentCustomer = () => {
     const token = Cookies.get("token");
@@ -18,9 +18,8 @@ const RecentCustomer = () => {
         { headers }
       )
       .then((response) => {
-        const firstCustomer = response?.data?.data?.[0];
-        setRecentCustomer(firstCustomer);
-        console.log(firstCustomer);
+        setRecentCustomer(response?.data?.data);
+        console.log(response?.data?.data);
       })
       .catch((error) => {
         console.log(error);
@@ -34,16 +33,23 @@ const RecentCustomer = () => {
   return (
     <div className="bg-white rounded-[8px] p-3">
       <h3 className="text-[20px] font-bold">Recent Customer</h3>
-      <div className="bg-cloud-burst-200 w-full h-auto p-3 rounded-[12px]">
-        <div className="flex items-center gap-4">
-          <div className="bg-cloud-burst-500 w-[30px] h-[30px] flex justify-center items-center text-white rounded-full">
-            <h5>{recentCustomer.username?.charAt(0).toUpperCase()}</h5>
-          </div>
-          <div>
-            <p>{recentCustomer.username}</p>
-            <p>{recentCustomer.provinceName}</p>
-          </div>
-        </div>
+      <div className="  flex flex-col gap-5">
+        {recentCustomer.slice(0, 4).map((item) => {
+          return (
+            <div
+              className=" bg-cloud-burst-200 w-full h-auto p-3 rounded-[12px] flex items-center gap-4 border border-black"
+              key={item.transactionId}
+            >
+              <div className="bg-cloud-burst-500 w-[30px] h-[30px] flex justify-center items-center text-white rounded-full">
+                <h5>{item.username.charAt(0).toUpperCase()}</h5>
+              </div>
+              <div className="">
+                <p>{item.username}</p>
+                <p>{item.regencyName}</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
       <button className="flex items-center gap-5">
         See More
