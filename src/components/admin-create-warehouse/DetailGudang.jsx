2e317b8d0613cee07876820b7bb37b52c123e07b
Ades <adesintaaaa@gmail.com>
2023-12-11 01:24:59 +0800

    update: create warehouse page

--- src/components/admin-create-warehouse/DetailGudang.jsx
+++ src/components/admin-create-warehouse/DetailGudang.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Peta from "../admin-create-warehouse/Peta";
 
 const DetailGudang = () => {
   const [provinsi, setProvinsi] = useState([]);
@@ -191,6 +192,7 @@ const DetailGudang = () => {
           placeholder="No. Telp"
         />
       </div>
+      <Peta/>
       <div className="mt-4">
         
       </div>
