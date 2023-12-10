2cf5bb45363da453cb19c4ec3fad488fdc920c1b
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-12-10 17:27:01 +0700

    consume endpoint dashboard total payment

--- src/components/admin-dashboard/CardTotal.jsx
+++ src/components/admin-dashboard/CardTotal.jsx
@@ -1,7 +1,37 @@
 import manageGudangIcon from "../../assets/manage-gudang-icon.svg";
 import transaksiIcon from "../../assets/transaksi-icon.svg";
+import Cookies from "js-cookie";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const CardTotal = () => {
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const handleCartTotal = () => {
+    const token = Cookies.get("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    axios
+      .get("https://digiwarehouse-app.onrender.com/dasboard/payment/total", {
+        headers,
+      })
+      .then((response) => {
+        setData(response?.data?.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    handleCartTotal();
+  }, []);
+
   return (
     <div
       id="card-wrapper"
@@ -10,19 +40,34 @@ const CardTotal = () => {
       <div className="bg-white py-6 px-5 rounded-[8px]">
         <img src={manageGudangIcon} alt="manage gudang icon" />
         <p className="text-[20px] my-[20px]">Total gudang yang tersedia</p>
-        <h3 className="font-bold text-[24px]">231</h3>
+        <h3 className="font-bold text-[24px]">
+          {" "}
+          {loading ? "Memuat data..." : data.totalWarehouseAvailabe}
+        </h3>
       </div>
       <div className="bg-white  py-6 px-5  rounded-[8px]">
         <img src={manageGudangIcon} alt="manage gudang icon" />
         <p className="text-[20px] my-[20px]">Total gudang yang disewa</p>
-        <h3 className="font-bold text-[24px]">231</h3>
+        <h3 className="font-bold text-[24px]">
+          {loading ? "Memuat data..." : data.totalWarehouseNotAvailable}
+        </h3>
       </div>
       <div className="bg-white  py-6 px-5  rounded-[8px]">
         <img src={transaksiIcon} alt="transaksi icon" />
         <p className="text-[20px] my-[20px]">Total pendapatan</p>
-        <h3 className="font-bold text-[24px]">Rp. 133.000.000</h3>
+        <h3 className="font-bold text-[24px]">
+          {loading
+            ? "Memuat data..."
+            : new Intl.NumberFormat("id-ID", {
+                style: "currency",
+                currency: "IDR",
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              }).format(data.totalPayment)}
+        </h3>
       </div>
     </div>
   );
 };
+
 export default CardTotal;
