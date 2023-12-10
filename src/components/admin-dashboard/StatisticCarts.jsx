0f4f1f2c120d2c7760eef29c9422c5cf25784e41
daffaliefalza <98019847+daffaliefalza@users.noreply.github.com>
2023-12-11 06:01:11 +0700

    consume api dashboars statictics & adding background color

--- src/components/admin-dashboard/StatisticCarts.jsx
+++ src/components/admin-dashboard/StatisticCarts.jsx
@@ -1,10 +1,62 @@
-import statistic from "../../assets/statistic.svg";
+import BarChart from "./BarChart";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-const StatisticCarts = () => {
+function StatisticCarts() {
+  const [userData, setUserData] = useState({
+    labels: [],
+    datasets: [
+      {
+        label: ["Total Pendapatan"],
+        data: [],
+        backgroundColor: ["#17345F"],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  const handleRecentCustomer = () => {
+    const token = Cookies.get("token");
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    axios
+      .get(
+        "https://digiwarehouse-app.onrender.com/dasboard/payment/statistic",
+        { headers }
+      )
+      .then((response) => {
+        const years = response?.data?.data.map((data) => data.year);
+        setUserData((prevUserData) => ({
+          ...prevUserData,
+          labels: years,
+          datasets: [
+            {
+              ...prevUserData.datasets[0],
+              data: response?.data?.data.map((data) => data.totalPayment),
+            },
+          ],
+        }));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    handleRecentCustomer();
+  }, []);
+
   return (
     <div>
-      <img src={statistic} alt="statistics" />
+      <div style={{ height: 350 }} className="bg-[#DDE5E9] p-3 rounded">
+        <BarChart chartData={userData} />
+      </div>
     </div>
   );
-};
+}
+
 export default StatisticCarts;
