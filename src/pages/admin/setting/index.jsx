e756956fc043eb54ead48ad58170bf32f2bc1275
Afitaganda <creatifypudding@gmail.com>
2023-12-10 20:03:26 +0700

    update pengaturan admin non-tabs

--- src/pages/admin/setting/index.jsx
+++ src/pages/admin/setting/index.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
 import Profile from '../../../components/admin-setting/Profile'
-import Security from '../../../components/admin-setting/Security'
 import SidebarAdmin from '../../../components/global-component/SidebarAdmin'
 import TopBar from '../../../components/global-component/TopBar'
 
 const AdminSetting = () => {
-  const [activeTab, setActiveTab] = useState('profile');
-
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
 
   return (
     <div className="bg-white">
@@ -17,34 +10,14 @@ const AdminSetting = () => {
         <SidebarAdmin />
         <div>
           <TopBar title={"Pengaturan Pengguna"} />
-          <div className="mx-[100px] max-w-screen-lg">
-            <div className=" text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-              <ul className="grid grid-cols-2 -mb-px">
-                <li>
-                  <a
-                    href="#"
-                    className={`w-full p-4 border-b-2 border-transparent rounded-t-lg hover:text-cloud-burst-600 hover:border-cloud-burst-600  ${activeTab === 'profile' ? 'text-cloud-burst-500 border-b-2 !border-cloud-burst-500' : ''
-                      }`}
-                    onClick={() => handleTabClick('profile')}
-                  >
-                    Profil Admin
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className={`w-full p-4 border-b-2 border-transparent rounded-t-lg hover:text-cloud-burst-600 hover:border-cloud-burst-600 ${activeTab === 'security' ? 'text-cloud-burst-500 border-b-2 !border-cloud-burst-500' : ''
-                      }`}
-                    onClick={() => handleTabClick('security')}
-                  >
-                    Keamanan Akun
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="my-5">
-              {activeTab === 'profile' && <Profile />}
-              {activeTab === 'security' && <Security />}
+          <div className="mx-8 max-w-screen-lg max-h-screen">
+            <div>
+              <div>
+                <h1
+                  className='py-3 font-bold text-cloud-burst-500 text-xl mb-8 mt-9'
+                >Pengaturan Akun</h1>
+              </div>
+              <Profile />
             </div>
           </div>
         </div>
