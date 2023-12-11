b8c76ac600c1174037b17172c7f96f5e5a2d2e71
Ades <adesintaaaa@gmail.com>
2023-12-11 23:21:36 +0800

    feat: search total nominal

--- src/components/admin-transaction/AllTransaction.jsx
+++ src/components/admin-transaction/AllTransaction.jsx
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cookies from "js-cookie";
 import ArrowBack from "../../assets/arrow-back-left-Icons.svg";
+import searchIcon from "../../assets/search-icon.svg";
 import ArrowNext from "../../assets/arrow-next-right-Icons.svg";
 
 const TransactionList = () => {
   const [transactionList, setTransactionList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [tempSearchTerm, setTempSearchTerm] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedFilter, setSelectedFilter] = useState("");
   const [isInputCleared, setIsInputCleared] = useState(false);
 
-  const handleAllTransaction = () => {
-    setLoading(true);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchTerm(tempSearchTerm);
+    setIsInputCleared(false);
+  };
 
+  const fetchTransactionData = () => {
+    setLoading(true);
     const token = Cookies.get("token");
     const headers = {
       Authorization: `Bearer ${token}`,
@@ -49,17 +56,16 @@ const TransactionList = () => {
       })
       .finally(() => {
         setLoading(false);
-        setIsInputCleared(false);
       });
   };
 
   useEffect(() => {
-    handleAllTransaction();
-  }, [searchTerm, selectedFilter]);
+    fetchTransactionData();
+  }, [searchTerm, selectedFilter, isInputCleared]);
 
   useEffect(() => {
     if (isInputCleared) {
-      handleAllTransaction();
+      fetchTransactionData();
     }
   }, [isInputCleared]);
 
@@ -86,16 +92,35 @@ const TransactionList = () => {
             </select>
           </div>
         </div>
-        <div className="flex items-center gap-x-8 md:gap-x-4 ">
+        <div className="flex items-center gap-x-8 md:gap-x-4">
           <h2 className="text-sm md:text-lg lg:text-[20px] font-bold text-cloud-burst-500">
             Total Tagihan
           </h2>
-          <input
-            className="shadow appearance-none border rounded-xl w-[200px] lg:w-[300px] h-[56px] py-2 px-3  font text-[#2C2C2E] leading-tight focus:outline-none focus:shadow-outline placeholder:text-[#2C2C2E]"
-            id="username"
-            type="text"
-            placeholder="113.700.000"
-          />
+          <form
+            onSubmit={handleSearch}
+            className="relative rounded-[28px] flex items-center"
+          >
+            <input
+              value={tempSearchTerm}
+              onChange={(e) => setTempSearchTerm(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch(e);
+                }
+              }}
+              className="shadow appearance-none border rounded-xl w-[200px] lg:w-[300px] h-[56px] py-2 px-3 font text-[#2C2C2E] leading-tight focus:outline-none focus:shadow-outline placeholder:text-[#2C2C2E]"
+              id="input-search"
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              type="submit"
+              id="button-search"
+              className="absolute right-0 top-0 mt-5 mr-4 cursor-pointer"
+            >
+              <img src={searchIcon} alt="" />
+            </button>
+          </form>
         </div>
       </div>
 
@@ -125,7 +150,12 @@ const TransactionList = () => {
                   <td className="pb-2">{item?.transactionDate}</td>
                   <td className="pb-2">{item?.paymentSchemeName}</td>
                   <td className="pb-2">{item?.username}</td>
-                  <td className="pb-2">Rp. {item?.nominal}</td>
+                  <td className="pb-2">
+                    {item?.nominal.toLocaleString("id-ID", {
+                      style: "currency",
+                      currency: "IDR",
+                    })}
+                  </td>
                 </tr>
               ))}
             </tbody>
