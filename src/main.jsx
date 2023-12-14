4bb3a94efe8d055feb42b220bf40b0ba92e5ef8c
Moh.afizan <alfijan12@gmail.com>
2023-12-14 21:29:37 +0800

    update cara ngambil token di komponen ListWarehouse

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import "slick-carousel/slick/slick.css"; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store.js";
+
+import "./index.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
