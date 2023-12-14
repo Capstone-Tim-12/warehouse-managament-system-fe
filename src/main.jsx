030cea948a59050fde5df91d08a30c23c6f01f5e
Moh.afizan <alfijan12@gmail.com>
2023-12-15 04:09:10 +0800

    update logic global state (store.js)

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store.js";
 
 import "./index.css";
 import "slick-carousel/slick/slick.css";
@@ -11,7 +12,9 @@ import "slick-carousel/slick/slick-theme.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store.js
+++ src/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storageSession from "redux-persist/lib/storage/session";
 import authReducer from "./features/authSlice";
 
+const persistConfig = {
+  key: "root",
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
 });
 
+export const persistor = persistStore(store);
 export default store;
