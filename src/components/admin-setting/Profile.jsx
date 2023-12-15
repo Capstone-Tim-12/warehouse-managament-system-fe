82d1c60ea110aaca1cfa1025faa88045df67ad57
Afitaganda <creatifypudding@gmail.com>
2023-12-15 18:57:11 +0700

    update: validating password

--- src/components/admin-setting/Profile.jsx
+++ src/components/admin-setting/Profile.jsx
@@ -5,6 +5,7 @@ import userIcon from '../../assets/user-setting-icon.svg'
 import emailIcon from '../../assets/icon-email-setting.svg'
 import passIcon from '../../assets/icon-pass-setting.svg'
 import Eye from "../../assets/eye-icon.svg";
+import EyeSlash from "../../assets/eye-slash-icon.svg";
 
 import { getToken } from '../../utils/Token'
 import Popup from '../global-component/Popup'
@@ -19,15 +20,14 @@ const headers = {
 const Profile = () => {
   const [isLoading, setLoading] = useState(false)
   const [isUpdating, setUpdating] = useState(false)
-  const [showPassword, setShowPassword] = useState(false);
+  const [isPasswordError, setIsPasswordError] = useState(false)
+  const [showPassword, setShowPassword] = useState(false)
 
   const [profile, setProfile] = useState({
     username: '',
     email: '',
     password: '',
   })
-
-  const isEmptyValue = Object.values(profile).some(val => val === null || val === '')
 
   const handleChange = (e) => {
     const { name, value } = e.target
@@ -53,18 +53,33 @@ const Profile = () => {
       .finally(() => setLoading(false))
   }
 
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const handleSubmit = () => {
     setUpdating(true)
     axios.put(endpointUpdate, profile, { headers })
       .then(() => alert('SUCCESS UPDATE USER'))
       .catch(() => alert('FAILED UPDATE USER'))
-      .finally(() => setUpdating(false))
+      .finally(() => {
+        setUpdating(false)
+        setIsPasswordError(false)
+        setShowPassword(false)
+      })
   }
+
+  const validate = (e) => {
+    e.preventDefault();
+
+    return !profile.password
+      ? setIsPasswordError(true)
+      : handleSubmit()
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    setIsPasswordError((isPasswordError && !profile.password))
+  }, [profile.password, isPasswordError])
 
   if (isLoading) return <p>Memuat data...</p>
 
@@ -109,43 +124,46 @@ const Profile = () => {
           </label>
         </div>
       </div>
-      <div className={`flex flex-row items-center m-auto px-4 gap-2 h-16 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.password !== '' && 'border-cloud-burst-500 border'}`}>
-        <div>
-          <img src={passIcon} alt='' />
-        </div>
-        <div className='flex flex-col w-full relative'>
-          <label className='relative cursor-pointer mt-1'>
-            <input
-              id='inpt-setting-secure-password'
-              name='password'
-              value={profile.password || ''}
-              onChange={handleChange}
-              placeholder=''
-              type={showPassword ? "text" : "password"}
-              className='focus:outline-none focus:ring-0 border border-none transition duration-200 placeholder-opacity-0 placeholder-gray-300'
-            />
-            <div
-              className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
-              onClick={togglePasswordVisibility}
-            >
-              <img
-                src={Eye}
-                alt="Toggle Password Visibility"
-                className="w-5 h-5"
+      <div className='flex flex-col gap-2'>
+        <div className={`flex flex-row items-center m-auto px-4 gap-2 h-16 rounded-lg w-full focus-within:border-cloud-burst-500 border ${profile.password !== '' && 'border-cloud-burst-500 border'}`}>
+          <div>
+            <img src={passIcon} alt='' />
+          </div>
+          <div className='flex flex-col w-full relative'>
+            <label className='relative cursor-pointer mt-1'>
+              <input
+                id='inpt-setting-secure-password'
+                name='password'
+                value={profile.password || ''}
+                onChange={handleChange}
+                placeholder=''
+                type={showPassword ? "text" : "password"}
+                className='focus:outline-none focus:ring-0 border border-none transition duration-200 placeholder-opacity-0 placeholder-gray-300'
               />
-            </div>            
-            <span className='absolute left-0 transition duration-200 label-input'>Password</span>
-          </label>
+              <div
+                className="absolute top-1/2 right-4 transform -translate-y-1/2 cursor-pointer"
+                onClick={togglePasswordVisibility}
+              >
+                <img
+                  src={showPassword ? EyeSlash : Eye}
+                  alt="Toggle Password Visibility"
+                  className="w-5 h-5"
+                />
+              </div>
+              <span className='absolute left-0 transition duration-200 label-input'>Password</span>
+            </label>
+          </div>
         </div>
+        <p className="text-red-400">{isPasswordError && 'Masukkan Password!'}</p>
       </div>
       <div className='flex justify-end'>
         <button
           id='save-profile-setting'
           data-modal-target="popup-loading"
           data-modal-toggle="popup-loading"
-          disabled={isEmptyValue && isUpdating}
-          onClick={handleSubmit}
-          className={`bg-crusta-500 text-white flex justify-center items-center h-10 w-72 rounded-lg hover:bg-crusta-600 ${isEmptyValue && 'bg-crusta-500 opacity-50 cursor-not-allowed'}`}
+          disabled={isUpdating || isPasswordError}
+          onClick={validate}
+          className={`bg-crusta-500 text-white flex justify-center items-center h-10 w-72 rounded-lg hover:bg-crusta-600 ${isUpdating || isPasswordError && 'bg-crusta-500 opacity-50 cursor-not-allowed'}`}
         >
           Simpan
         </button>
